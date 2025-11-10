5acfb3da1c773e4edf5787014ea41edf28672ebb
Ruby.abim <abimfebriansyah2000@gmail.com>
2025-11-10 15:16:26 +0700

    Menambahkan code export const login

--- backend/Src/controllers/authController.ts
+++ backend/Src/controllers/authController.ts
@@ -4,3 +4,11 @@ import jwt from 'jsonwebtoken';
 import { prisma } from '../utils/database';
 import { LoginRequest, AuthResponse } from '../types';
 const JWT_SECRET = process.env.JWT_SECRET || 'warung-ibuk-iyos-secret-key';
+
+export const login = async (req: Request, res: Response) =>{
+
+
+  try {
+
+  }
+}
