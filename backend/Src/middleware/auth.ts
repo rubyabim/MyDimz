2cb98f57e35170d7123dd41d4f548fbe8f6e9a47
Ruby.abim <abimfebriansyah2000@gmail.com>
2025-11-10 15:05:01 +0700

    Memambahkan code export const requireAdmin

--- backend/Src/middleware/auth.ts
+++ backend/Src/middleware/auth.ts
@@ -18,5 +18,9 @@ export const authenticateToken = (req: Request, res: Response, next: NextFunctio
   });
 };
 
+export const requireAdmin = (req: Request, res: Response, next: NextFunction) => {
+
+
+}
 
 
