c080eabd4bc73551e10d5e66dc2e813e7cc3999e
Ruby.abim <abimfebriansyah2000@gmail.com>
2025-11-10 15:01:03 +0700

    Memambahkan code export const authenticateToken

--- backend/Src/middleware/auth.ts
+++ backend/Src/middleware/auth.ts
@@ -1,3 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 const JWT_SECRET = process.env.JWT_SECRET || 'warung-ibuk-iyos-secret-key';
+export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+}
