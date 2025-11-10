545b697ef10c90809c2bce6fb8a1b8e807bb364e
Ruby.abim <abimfebriansyah2000@gmail.com>
2025-11-10 15:03:07 +0700

    Memambahkan code        if (!token)

--- backend/Src/middleware/auth.ts
+++ backend/Src/middleware/auth.ts
@@ -3,17 +3,11 @@ import jwt from 'jsonwebtoken';
 const JWT_SECRET = process.env.JWT_SECRET || 'warung-ibuk-iyos-secret-key';
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
      const authHeader = req.headers['authorization'];
-       const token = authHeader && authHeader.split(' ')[1];
+       const token = authHeader && authHeader.split(' ')[1]
 
-
-
-
-
-
-
-
-
-
+       if (!token) {
+    return res.status(401).json({ error: 'Access token required' });
+}
 
 
 }
