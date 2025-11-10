1d762affa518a637bb69fa669dbfb25f9bfff70f
Ruby.abim <abimfebriansyah2000@gmail.com>
2025-11-10 13:18:18 +0700

    Menambahkan code explor interface Product

--- backend/Src/types/index.ts
+++ backend/Src/types/index.ts
@@ -6,3 +6,16 @@ export interface User {
   createdAt: Date;
   updatedAt: Date;
 }
+
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  stock: number;
+  category: string;
+  image?: string;
+  description?: string;
+  barcode?: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
