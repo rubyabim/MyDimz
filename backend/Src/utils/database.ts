4bcafb4e205eb9652f4d7580460c5f55d22c6423
Ruby.abim <abimfebriansyah2000@gmail.com>
2025-11-10 14:42:00 +0700

    Menambahkan code export const prisma = globalForPrisma.prisma || new PrismaClient();

--- backend/Src/utils/database.ts
+++ backend/Src/utils/database.ts
@@ -1,2 +1,3 @@
 import { PrismaClient } from '@prisma/client';
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
+export const prisma = globalForPrisma.prisma || new PrismaClient();
