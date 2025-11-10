018133ce3a21397fffc28f715e3b7363e0bd9d30
Ruby.abim <abimfebriansyah2000@gmail.com>
2025-11-10 15:02:11 +0700

    Memambahkan code    const token = authHeader && authHeader.split(' ')[1];

--- backend/Src/middleware/auth.ts
+++ backend/Src/middleware/auth.ts
@@ -3,6 +3,7 @@ import jwt from 'jsonwebtoken';
 const JWT_SECRET = process.env.JWT_SECRET || 'warung-ibuk-iyos-secret-key';
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
      const authHeader = req.headers['authorization'];
+       const token = authHeader && authHeader.split(' ')[1];
 
 
 
