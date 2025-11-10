66d8c3913c77f262667be9054ded224dc7d59d70
Ruby.abim <abimfebriansyah2000@gmail.com>
2025-11-10 15:15:00 +0700

    Menambahkan code const JWT_SECRET = process.env.JWT

--- backend/Src/controllers/authController.ts
+++ backend/Src/controllers/authController.ts
@@ -3,3 +3,4 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { prisma } from '../utils/database';
 import { LoginRequest, AuthResponse } from '../types';
+const JWT_SECRET = process.env.JWT_SECRET || 'warung-ibuk-iyos-secret-key';
