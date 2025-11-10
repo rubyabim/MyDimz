ca9b01238dd55699f6932146d2b3f9823ac9dcfc
Ruby.abim <abimfebriansyah2000@gmail.com>
2025-11-10 15:05:44 +0700

    Memambahkan code if (req.user?.role

--- backend/Src/middleware/auth.ts
+++ backend/Src/middleware/auth.ts
@@ -19,7 +19,9 @@ export const authenticateToken = (req: Request, res: Response, next: NextFunctio
 };
 
 export const requireAdmin = (req: Request, res: Response, next: NextFunction) => {
+  if (req.user?.role !== 'admin') {
 
+  }
 
 }
 
