c732f99c44cf07cadfe777df284354f261698f56
Ruby.abim <abimfebriansyah2000@gmail.com>
2025-11-10 14:39:07 +0700

    Menambahkan code Declare global

--- backend/Src/types/index.ts
+++ backend/Src/types/index.ts
@@ -67,3 +67,15 @@ export interface SaleRequest {
   }>;
   cashier: string;
 }
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: number;
+        username: string;
+        role: string;
+      };
+    }
+  }
+}
