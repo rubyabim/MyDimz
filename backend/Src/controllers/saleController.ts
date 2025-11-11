b67fecc3b903b195a306d8946ed21352a6199b4c
Ruby.abim <abimfebriansyah2000@gmail.com>
2025-11-11 17:34:36 +0700

    Menambahkan code export const createSale

--- backend/Src/controllers/saleController.ts
+++ backend/Src/controllers/saleController.ts
@@ -1,3 +1,7 @@
 import { Request, Response } from 'express';
 import { prisma } from '../utils/database';
 import { Sale, SaleRequest } from '../types';
+
+export const createSale = async (req: Request, res: Response) => {
+
+}
