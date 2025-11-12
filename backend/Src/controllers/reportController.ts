a364a11024765042861ec5479184817309742636
Ruby.abim <abimfebriansyah2000@gmail.com>
2025-11-12 20:00:33 +0700

    Menambahkan code const totalsale

--- backend/Src/controllers/reportController.ts
+++ backend/Src/controllers/reportController.ts
@@ -46,3 +46,5 @@ export const generateDailyReport = async (req: Request, res: Response) => {
 
           doc.fontSize(14).text('Ringkasan Penjualan:', 100, y);
            y += 30;
+
+           const totalSales = sales.reduce((sum: number, sale: Sale) => sum + sale.total, 0);
