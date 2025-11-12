fee8c1cf8125fe548f3529cd9b122592619caf75
Ruby.abim <abimfebriansyah2000@gmail.com>
2025-11-12 19:25:38 +0700

    Menambahkan code    if (!date)

--- backend/Src/controllers/reportController.ts
+++ backend/Src/controllers/reportController.ts
@@ -6,5 +6,11 @@ import { Sale } from '../types';
 export const generateDailyReport = async (req: Request, res: Response) => {
     try {
     const { date } = req.query;
+
+
+       if (!date) {
+      return res.status(400).json({ error: 'Date parameter is required' });
 }
+
+
 }
