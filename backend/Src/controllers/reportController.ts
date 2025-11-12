489268cabc0507dbe38d2c568c8e44d532b3babe
Ruby.abim <abimfebriansyah2000@gmail.com>
2025-11-12 19:53:23 +0700

    Menambahkan code  doc fontsize

--- backend/Src/controllers/reportController.ts
+++ backend/Src/controllers/reportController.ts
@@ -38,3 +38,4 @@ export const generateDailyReport = async (req: Request, res: Response) => {
       doc.pipe(res);
 
       doc.fontSize(20).text('WARUNG IBUK IYOS', 100, 100);
+      doc.fontSize(16).text('Laporan Harian Penjualan', 100, 130);
