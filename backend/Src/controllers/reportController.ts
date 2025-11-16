b6a11e5e33975e06f0098604cbd64271130aa3db
Ruby.abim <abimfebriansyah2000@gmail.com>
2025-11-16 19:37:54 +0700

    Menambahkan code  const totalTransactions = sales.length;

--- backend/Src/controllers/reportController.ts
+++ backend/Src/controllers/reportController.ts
@@ -238,3 +238,4 @@ export const generateMonthlyReport = async (req: Request, res: Response) => {
            doc.fontSize(14).text('Ringkasan Bulanan:', 100, y);
     y += 30;
    const totalSales = sales.reduce((sum: number, sale: Sale) => sum + sale.total, 0);
+    const totalTransactions = sales.length;
