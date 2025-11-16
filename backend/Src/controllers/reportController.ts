53ca7af253c64c2b408142bfbccb21f051f279e2
Ruby.abim <abimfebriansyah2000@gmail.com>
2025-11-16 19:27:48 +0700

    Menambahkan code  let y = 220;

--- backend/Src/controllers/reportController.ts
+++ backend/Src/controllers/reportController.ts
@@ -232,3 +232,5 @@ export const generateMonthlyReport = async (req: Request, res: Response) => {
          doc.fontSize(12).text(`Periode: ${new Date(parseInt(year as string), parseInt(month as string) - 
           1).toLocaleDateString('id-ID', { month: 'long', year: 'numeric' })}`, 100, 160);
           doc.text(`Dicetak pada: ${new Date().toLocaleDateString('id-ID')}`, 100, 180);
+
+           let y = 220;
