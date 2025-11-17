bac78d6e49bb48d2a2b0f7d0b9f4c911b6f6d73c
Ruby.abim <abimfebriansyah2000@gmail.com>
2025-11-17 17:25:59 +0700

    Menambahkan code from '../controllers/saleController'

--- backend/Src/routes/index.ts
+++ backend/Src/routes/index.ts
@@ -14,6 +14,7 @@ import {  login,
           } from '../controllers/productController';
           import {  
             createSale, 
-              getSales 
+              getSales
+              from '../controllers/saleController';
 }
 
