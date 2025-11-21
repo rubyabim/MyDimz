002a52a3aa3db3b53a00126a4aaebb26383776ef
dzilan <radityadzilan@gmail.com>
2025-11-21 23:39:23 +0700

    menambahkan code {children}

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout ({
 }) {
   return (
     <html lang="id"></html>
-    <body className={inter.className}></body>
+    <body className={inter.className}>
+      {children}
   )
 }
