8bdae35cd746f143038d0589daf07100ccf5b060
dzilan <radityadzilan@gmail.com>
2026-01-13 00:21:58 +0700

    Menambahkan Komentar Mengatur halaman detail dengan parameter dinamis [id]

--- mobile/app/products/_layout.tsx
+++ mobile/app/products/_layout.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 export default function ProductsLayout() {
   return (
     <Stack>
+      {/* Mengatur halaman detail dengan parameter dinamis [id] */}
       <Stack.Screen 
         name="[id]" 
         options={{ 
