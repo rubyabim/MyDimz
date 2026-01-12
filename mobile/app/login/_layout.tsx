ba95864bdb1310c74d03d43c2ed31274f862b2d9
dzilan <radityadzilan@gmail.com>
2026-01-13 00:06:18 +0700

    Menambahkan komentar Mengatur halaman utama (index) di dalam folder login

--- mobile/app/login/_layout.tsx
+++ mobile/app/login/_layout.tsx
@@ -5,6 +5,7 @@ export default function LoginLayout() {
   return (
     // Stack: Wadah untuk tumpukan halaman navigasi
     <Stack>
+      {/* Mengatur halaman utama (index) di dalam folder login */}
       <Stack.Screen 
         name="index" 
         options={{ 
