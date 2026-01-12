36323810bed7ace3a0b21ccf252ff908b93681ee
dzilan <radityadzilan@gmail.com>
2026-01-13 00:05:56 +0700

    Menambahkan komentar Stack: Wadah untuk tumpukan halaman navigasi

--- mobile/app/login/_layout.tsx
+++ mobile/app/login/_layout.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 export default function LoginLayout() {
   return (
+    // Stack: Wadah untuk tumpukan halaman navigasi
     <Stack>
       <Stack.Screen 
         name="index" 
