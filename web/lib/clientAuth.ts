cc622d40648b4e74d2b2248ffdc3d159c648a937
Ruby.abim <abimfebriansyah2000@gmail.com>
2026-01-13 12:17:50 +0700

    menambahkan // Diperlukan  karena menggunakan  localStorage

--- web/lib/clientAuth.ts
+++ web/lib/clientAuth.ts
@@ -1,6 +1,6 @@
 "use client";
 // Menandakan bahwa file ini dijalankan di sisi client (browser)
-// Diperlukan karena menggunakan 
+// Diperlukan karena menggunakan localStorage 
 const TOKEN_KEY = "wiibuk_token";
 
 export function setAuthToken(token: string) {
