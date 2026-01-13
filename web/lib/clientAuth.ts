3620932e06b674858d67ef372f00e3d7daa73f82
Ruby.abim <abimfebriansyah2000@gmail.com>
2026-01-13 12:20:56 +0700

    menambahkan note // Menyimpan token   autentikasi  ke

--- web/lib/clientAuth.ts
+++ web/lib/clientAuth.ts
@@ -3,7 +3,7 @@
 // Diperlukan karena menggunakan localStorage dan window
 // Key untuk menyimpan token di localStorage
 const TOKEN_KEY = "wiibuk_token";
-// Menyimpan token autentikasi 
+// Menyimpan token autentikasi  ke 
 export function setAuthToken(token: string) {
   if (typeof window === "undefined") return;
   localStorage.setItem(TOKEN_KEY, token);
