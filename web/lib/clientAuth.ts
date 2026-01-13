9efb9ac8a3c463956f32fd8d9e1c7ef9ee7bdea0
Ruby.abim <abimfebriansyah2000@gmail.com>
2026-01-13 12:44:17 +0700

    menambahkan note // Mengecek  apakah user memiliki  role

--- web/lib/clientAuth.ts
+++ web/lib/clientAuth.ts
@@ -52,7 +52,7 @@ export async function getUserProfile() {
     return null;
   }
 }
-// Mengecek apakah user memiliki 
+// Mengecek apakah user memiliki role 
 export function isAdmin(user: any): boolean {
   return user?.role === "admin" || user?.role === "seller";
 }
