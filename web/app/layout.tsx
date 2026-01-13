c07a57b184fecd470d667e5a8437785df0929e1a
Ruby.abim <abimfebriansyah2000@gmail.com>
2026-01-13 09:47:43 +0700

    membuat note // Berfungsi

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -7,7 +7,7 @@ import type { Metadata } from 'next';
 // Biasanya berisi reset  CSS , font, dan variabel  warna
 import './globals.css';
 // Import CartProvider
-//
+// Berfungsi 
 
 import { CartProvider } from './components/CartContext';
 
