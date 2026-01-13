07578ad4704af6b2a3a91c13b9234c68c3c1d228
dzilan <radityadzilan@gmail.com>
2026-01-13 18:18:57 +0700

    menambahkan komentar Menggunakan POST karena kita mengirim data baru untuk disimpan

--- web/app/cart/page.tsx
+++ web/app/cart/page.tsx
@@ -35,6 +35,7 @@ export default function CartPage() {
         // Mengambil alamat server dari variabel lingkungan atau default ke localhost
         `${process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:500/api"}/sales`,
         {
+          // Menggunakan POST karena kita mengirim data baru untuk disimpan
           method: "POST",
           headers: {
             "Content-Type": "application/json",
