3068e6ad742606373e9923bd42ce45236bd40fed
Ruby.abim <abimfebriansyah2000@gmail.com>
2026-01-13 09:33:13 +0700

    membuat note // Import

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -3,7 +3,7 @@
 // Digunakan untuk mengatur SEO seperti title dan description
 
 import type { Metadata } from 'next';
-// 
+// Import
 import './globals.css';
 import { CartProvider } from './components/CartContext';
 
