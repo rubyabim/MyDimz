1f5bf5839bfcaa7d6097d88f1ad714675688184e
Ruby.abim <abimfebriansyah2000@gmail.com>
2026-01-13 12:28:25 +0700

    menambhakan note // Cegah

--- web/lib/clientAuth.ts
+++ web/lib/clientAuth.ts
@@ -18,7 +18,7 @@ export function getAuthToken(): string | null {
 }
 // Menghapus token autentikasi (logout)
 export function clearAuthToken() {
-  // 
+  // Cegah 
   if (typeof window === "undefined") return;
   localStorage.removeItem(TOKEN_KEY);
 }
