12bf72cf5a7e436813fe30464ed1c43a734dfc91
Ruby.abim <abimfebriansyah2000@gmail.com>
2026-01-13 09:55:30 +0700

    menambahkan note // Semua  halaman  akan  dibungkus  oleh

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
   description: 'Belanja kebutuhan sehari-hari dengan mudah', // Deskripsi website 
 };
 // RootLayout adalah  layout utama  aplikasi
-// Semua  halaman  akan dibungkus 
+// Semua  halaman  akan dibungkus oleh 
 export default function RootLayout({
   children,
 }: {
