e61750fb86adab37d772ce465e080c2f2809d273
Ruby.abim <abimfebriansyah2000@gmail.com>
2026-01-13 09:53:19 +0700

    menambahkan note //

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   title: 'Toko Kelontong MyDimz',   // Judul website
   description: 'Belanja kebutuhan sehari-hari dengan mudah', // Deskripsi website 
 };
-
+// 
 export default function RootLayout({
   children,
 }: {
