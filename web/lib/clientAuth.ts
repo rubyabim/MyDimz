90acd7a7ec6e7c4e49605d6131ff36ddf4c4f994
Ruby.abim <abimfebriansyah2000@gmail.com>
2026-01-13 12:33:24 +0700

    menambahkan note // Jika

--- web/lib/clientAuth.ts
+++ web/lib/clientAuth.ts
@@ -28,7 +28,7 @@ export async function getUserProfile() {
     // Ambil token dari localStorage
     const token = getAuthToken();
 
-    // 
+    // Jika 
     if (!token) return null;
 
     const response = await fetch(
