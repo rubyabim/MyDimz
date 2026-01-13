f9873f1d9491bd42d87528dea58f1950a9b54eef
Ruby.abim <abimfebriansyah2000@gmail.com>
2026-01-13 09:45:21 +0700

    membuat note // Biasanya berisi  reset  CSS , font,

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -4,7 +4,7 @@
 
 import type { Metadata } from 'next';
 // Import global CSS
-// Biasanya berisi reset  CSS
+// Biasanya berisi reset  CSS , font,
 import './globals.css';
 import { CartProvider } from './components/CartContext';
 
