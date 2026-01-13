e0207df3341306925d7cb767d07298f4a1170dae
Ruby.abim <abimfebriansyah2000@gmail.com>
2026-01-13 09:51:32 +0700

    membuat note // Akan  otomatis  digunakan  oleh  Next.js

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -11,7 +11,7 @@ import './globals.css';
 
 import { CartProvider } from './components/CartContext';
 // Metadata website (SEO)
-// Akan  otomatis  digunakan  oleh 
+// Akan  otomatis  digunakan  oleh  Next.js
 export const metadata: Metadata = {
   title: 'Toko Kelontong MyDimz',
   description: 'Belanja kebutuhan sehari-hari dengan mudah',
