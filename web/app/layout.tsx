5b00198e3a30ff60f18934111a00f0c9bc3c71fe
Ruby.abim <abimfebriansyah2000@gmail.com>
2026-01-13 09:45:44 +0700

    membuat note // Biasanya berisi  reset  CSS , font, dan  variabel

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -4,7 +4,7 @@
 
 import type { Metadata } from 'next';
 // Import global CSS
-// Biasanya berisi reset  CSS , font, dan 
+// Biasanya berisi reset  CSS , font, dan variabel  
 import './globals.css';
 import { CartProvider } from './components/CartContext';
 
