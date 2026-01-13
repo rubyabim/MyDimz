6bcff69a4a29613a3e4c79983691cf0f1dbcd68d
Ruby.abim <abimfebriansyah2000@gmail.com>
2026-01-13 12:34:29 +0700

    menambahkan note // Jika token tidak ada, user dianggap belum

--- web/lib/clientAuth.ts
+++ web/lib/clientAuth.ts
@@ -28,7 +28,7 @@ export async function getUserProfile() {
     // Ambil token dari localStorage
     const token = getAuthToken();
 
-    // Jika token tidak ada, user dianggap 
+    // Jika token tidak ada, user dianggap belum 
     if (!token) return null;
 
     const response = await fetch(
