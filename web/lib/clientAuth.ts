d4a2ba04dfbaceea2daf90967301f429cac0da51
Ruby.abim <abimfebriansyah2000@gmail.com>
2026-01-13 12:32:22 +0700

    menambahkan note // Ambil

--- web/lib/clientAuth.ts
+++ web/lib/clientAuth.ts
@@ -25,7 +25,7 @@ export function clearAuthToken() {
 // Mengambil data profil user  yang sedang login Menggunakan endpoint /auth/me
 export async function getUserProfile() {
   try {
-    // 
+    // Ambil 
     const token = getAuthToken();
     if (!token) return null;
 
