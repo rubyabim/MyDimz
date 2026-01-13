756e0c59cd810ce1aa3536e64d87382e0dcb1645
Ruby.abim <abimfebriansyah2000@gmail.com>
2026-01-13 09:48:52 +0700

    membuat note // Berfungsi  sebagai  Context  untuk  state  keranjang  belanja

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -7,7 +7,7 @@ import type { Metadata } from 'next';
 // Biasanya berisi reset  CSS , font, dan variabel  warna
 import './globals.css';
 // Import CartProvider
-// Berfungsi  sebagai Context untuk state keranjang 
+// Berfungsi  sebagai Context untuk state keranjang belanja
 
 import { CartProvider } from './components/CartContext';
 
